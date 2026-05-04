90cf224d5351768bf140ff82ca747dacd1b80b63
abdikadir-mohamed-dev <abdikadirmohammed180@gmail.com>
2026-05-04 09:12:19 +0300

    fix: resolved test errors (defaultImage, defaultDate)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
+const defaultImage = "https://via.placeholder.com/215";
 function About({image, about}) {
     return (
         <aside>
-            <img src={image} alt="blog logo" />
+            <img src={image || defaultImage} alt="blog logo" />
             <p>{about}</p>
         </aside>
     );
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,8 +1,9 @@
+const defaultDate = "January 1, 1970";
 function Article({ title, date,preview }) {
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date || defaultDate}</small>
             <p>{preview}</p>
         </article>
     );
